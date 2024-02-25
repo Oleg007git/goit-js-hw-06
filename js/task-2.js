f295c91d1919c05a0e8781cb3d735e7610bd9fe2
Oleg007git <cusniroleg@icloud.com>
2024-02-25 10:51:59 -0800

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,24 +1,23 @@
-class Storage {
-  #items;
-  constructor(items) {
-    this.#items = items;
-  }
-  getItems() {
-    return this.#items;
-  }
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-  removeItem(itemToRemove) {
-    const deleteItems = this.#items.indexOf(itemToRemove);
-    if (deleteItems !== -1) {
-      this.#items.splice(deleteItems, 1);
-    }
-  }
-}
-const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem('Droid');
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem('Prolonger');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+const galleryList = document.createElement('ul.gallery');
+const images = [
+  {
+    url: '<https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260>',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: '<https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260>',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: '<https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260>',
+    alt: 'Group of Horses Running',
+  },
+];
+images.forEach((image) => {
+  const listItems = document.createElement('li');
+  const imageEl = document.createElement('img');
+  imageEl.src = images.url;
+  imageEl.alt = images.alt;
+  listItems.elem.append(imageEl);
+  galleryList.elem.append(listItems);
+});
